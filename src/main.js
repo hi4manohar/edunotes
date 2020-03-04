6d83319a8127db081463597a8398e3f422e5bb79
Manohar <hi4manohar@gmail.com>
2020-03-04 20:36:09 +0530

    added vuetify

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+import vuetify from "./plugins/vuetify";
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
