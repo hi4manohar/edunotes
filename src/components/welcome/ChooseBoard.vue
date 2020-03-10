afab98cbd1dc56df87b802c8bf7dbb73cf68d2e1
Manohar <hi4manohar@gmail.com>
2020-03-10 21:54:14 +0530

    changed config

--- src/components/welcome/ChooseBoard.vue
+++ src/components/welcome/ChooseBoard.vue
@@ -41,7 +41,7 @@ export default {
     loadBoard() {
       this.items = [
         {
-          avatar: this.publicPath + "Bordicon/CBSE.jpeg",
+          avatar: this.publicPath + "Bordicon/CBSE.jpg",
           title: "CBSE",
           subtitle: "Central Board "
         },
