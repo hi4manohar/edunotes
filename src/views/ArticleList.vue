6aa195188ebf12a133fad007986e36374f498fe4
Manohar <hi4manohar@gmail.com>
2020-03-21 23:15:11 +0530

    updated list

--- src/views/ArticleList.vue
+++ src/views/ArticleList.vue
@@ -20,11 +20,48 @@
             </v-list-item-content>
           </v-list-item>
         </div>
+        <v-container class="mb-12" v-else>
+          <h4>Article List</h4>
+          <br>
+
+          <div
+            class="list-card"
+            v-for="(name, index) in names"
+            @click="showContent(index)"
+            :key="index"
+          >
+            <v-card max-width="344" :class="{ active: activeIndex === index }">
+              <v-list-item v-ripple>
+                <v-list-item-content>
+                  <v-list-item-title class="subtitle-2" v-text="name.post_title"></v-list-item-title>
+                  <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="120"></v-img>
+              <v-card-text
+                v-html="trimmedData(name.post_content)">
+              </v-card-text>
+              <v-card-actions>
+                <v-btn text color="deep-purple accent-4">
+                  Read
+                </v-btn>
+                <v-btn text color="deep-purple accent-4">Save</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon>mdi-heart</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>mdi-share-variant</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </div>
+        </v-container>
         <div
           v-for="(name, index) in names"
           @click="showContent(index)"
           :key="index"
-          v-else
+          v-show="false"
         >
           <v-list-item :class="{ active: activeIndex === index }" ripple>
             <v-list-item-content>
@@ -43,37 +80,8 @@
               </v-btn>
             </v-list-item-action>
           </v-list-item>
-        </div>
-      </v-content>
-      <v-container class="mb-12">
-        <h4>Article List</h4>
-        <br>
-        <v-card max-width="344">
-          <v-list-item v-ripple>
-            <v-list-item-content>
-              <v-list-item-title class="subtitle-2" >Our Changing Planet</v-list-item-title>
-              <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="120"></v-img>
-          <v-card-text>
-            Visit ten places on our planet that are undergoing the biggest changes today.
-          </v-card-text>
-          <v-card-actions>
-            <v-btn text color="deep-purple accent-4">
-              Read
-            </v-btn>
-            <v-btn text color="deep-purple accent-4">Save</v-btn>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-              <v-icon>mdi-heart</v-icon>
-            </v-btn>
-            <v-btn icon>
-              <v-icon>mdi-share-variant</v-icon>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-container>
+        </div>        
+      </v-content>      
       <Footer active="home" />
     </div>
     <div v-if="articleContent">
