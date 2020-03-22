8ba2df950be28dcf1cf8aff69dc2100eff040d65
Pradeep <hi4manohar@gmail.com>
2020-03-22 18:54:10 +0530

    syllabus chapter page

--- src/views/SubjectPosts.vue
+++ src/views/SubjectPosts.vue
@@ -10,25 +10,18 @@
       <v-btn icon light>
         <v-icon color="grey darken-2">mdi-arrow-left</v-icon>
       </v-btn>
-      <v-toolbar-title>Choose Chapter</v-toolbar-title>
+      <v-toolbar-title class="pl-1">Choose Chapter</v-toolbar-title>
       <v-spacer></v-spacer>
     </v-app-bar>
-    <div>
-      <v-sheet
-        class="mx-auto mt-10"
-        max-width="400"
-      >
-        <v-slide-group>
-          <v-slide-item
-            v-for="n in 15"
-            :key="n">
-            <v-card
-              class="ma-3"
+    <div class="slidercon mt-10">
+      <div class="sliderconin" :style="{ width: getsliderWidth }">
+        <v-card
               :elevation="5"
-              max-width="260"
+              width="240"
+              v-for="(item, index) in 10" :key="index"
             >
               <v-img
-                height="200"
+                height="150"
                 src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
               ></v-img>
               <v-card-title class="py-2">01 Chapter</v-card-title>
@@ -39,9 +32,8 @@
                 <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
               </v-card-text>
             </v-card>
-          </v-slide-item>
-        </v-slide-group>
-      </v-sheet>
+        
+      </div>
     </div>
     </v-container>
   </v-app>
@@ -50,10 +42,24 @@
 <script>
 export default {
   data: () => ({
-
+    sliderWidth: '260',
+    itemLength: 10,
   }),
+  computed: {
+    getsliderWidth() {
+      return this.itemLength * this.sliderWidth + 'px';
+    }
+  }
 };
 </script>
 <style scoped="">
-  
+  .slidercon{
+    max-width:94vh;
+    overflow: auto;
+    margin:0 auto;
+  }
+  .v-card{
+    display: inline-block !important;
+    margin:10px !important;
+  }
 </style>
